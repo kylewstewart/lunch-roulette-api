aa5e7e91f82b34ca040b80a37012d1a573886a4a
kylewstewart <kyle.w.stewart@mac.com>
2017-06-19 17:29:11 -0400

    mailer

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
   namespace :v1 do
-    get '/users/', to: 'users#authorization_code'
+    post '/users/', to: 'users#authorization_code'
     resources :places do
       resources :reviews
     end
